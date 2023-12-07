4f7109b33f719c2fa0eb4c027f5947bcb4ae8c60
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2023-12-07 10:25:17 -1000

    Update LessonOne_HookProject.py

--- IOLANI_S24/LessonOne_HookProject.py
+++ IOLANI_S24/LessonOne_HookProject.py
@@ -1,1 +1,51 @@
+#USE THIS AS AN ICE BREAKER PROJECT TO SHOW THE KIDS A FUNNY BEGINNER PYTHON PROJECT
+
+import os, time
+
+# QUESTION 1
+input("Hello, I am the world's greatest fortune teller. Ask me a yes or no question. Ask me anything. I will reveal the truth!\n>")
+#Input "Is this class going to be awesome?"
+
+print("I can say with 100% certainty - yes")
+time.sleep(5)
+clear()
+
+# QUESTION 2
+input("Ask me something else!\n>")
+#Input "Is Mr. Stephen cool?"
+
+print("Hmmm...I don't think so. Better luck next time...")
+time.sleep(5)
+clear()
+
+# QUESTION 3
+input("Go ahead. Ask me something else.\n>")
+#Input "Is coding awesome?"
+
+print("Totally!")
+time.sleep(5)
+clear()
+
+# QUESTION 4 (HAVE STUDENTS ASK)
+input("What else would you like to know?>\n>")
+#Input student's response. We will play a joke on the student
+
+print("How am I supposed to know the answer to that! I am not a fortune teller!")
+time.sleep(5)
+print("Oh wait, yes I am a fortune teller. Shoot let me think")
+time.sleep(5)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print("Yes?")
+time.sleep(3)
+clear()
+
+# QUESTION 5 (HAVE STUDENTS ASK)
+input("OK OK. One last question.\n>")
+
+print("Uh oh I think I hear Dr. Cotrell coming. Goodbye!")
 
