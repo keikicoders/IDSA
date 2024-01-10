8403d293589c060b0134003208716373b82e8e52
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-09 19:05:15 -1000

    Update PasswordLogin.py

--- IOLANI_S24/LessonSix/PasswordLogin.py
+++ IOLANI_S24/LessonSix/PasswordLogin.py
@@ -1,1 +1,11 @@
+username = "keikicoders"
+password = "ilovecoding"
 
+print("Welcome to the best app in the world. In order to enter you must enter your username and passoword")
+user_input_username = input("Username: ")
+user_input_password = input("Password: ")
+
+if user_input_username == username and user_input_password == password:
+  print("Access granted!")
+else:
+  print("Access denied")
