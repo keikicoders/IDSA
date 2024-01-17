c1892c3b9e7e2c6206aad01aca6dcd3724261912
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-17 10:35:46 -1000

    Update CharacterCreator.py

--- IOLANI_S24/LessonSeven/CharacterCreator.py
+++ IOLANI_S24/LessonSeven/CharacterCreator.py
@@ -1,8 +1,26 @@
 import time, random
+username = 'NOTHING'
+password = ''
+
+def register():
+    username = input("Create a username: ")
+    password = input("Create a password: ")
+
+def login():
+  while True:
+    uInput = input("What is your username?:")
+    pInput = input("What is your password?:")
+    if uInput == username and pInput == password:
+      print("Success")
+      break
+    else:
+      print("I don't recognize that username or password")
+      continue
 
 def rollDice(side):
   result = random.randint(1,side)
   return result
+
 def health_roll ():
   health = (rollDice(6) * rollDice(12))/2+10
   return health
@@ -12,7 +30,17 @@ def attack_roll():
   return attack
 
 while True:
-  build = int(input('Welcome to the Legend Builder! \n\n Press 1 to build a new warrior \n Press 2 to exit\n\n >> '))
+  menu = int(input('Welcome to the Legend Builder! Please \n\n Press 1 to Login \n Press 2 to create a new account\n\n >>'))
+  if menu == 1:
+    login()
+  elif menu == 2:
+    register()
+  else:
+    print("Please submit valid response")
+    continue
+
+
+  build = int(input('Welcome to the Legend Builder! Please \n\n Press 1 to build a new warrior \n Press 2 to exit\n\n >> '))
   if build ==1:
     name = input('What is the name of your legend? >> ')
     type = input('Human, elf, wizard, or orc >> ')
