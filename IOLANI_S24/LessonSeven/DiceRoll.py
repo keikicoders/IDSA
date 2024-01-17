d6a09c9fa17f87af1fcdc7b94a2bbbdbd3d3b23d
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-17 13:26:49 -1000

    Update DiceRoll.py

--- IOLANI_S24/LessonSeven/DiceRoll.py
+++ IOLANI_S24/LessonSeven/DiceRoll.py
@@ -8,3 +8,6 @@ def roll_6_and_8():
   health = roll_6_sided_di * roll_8_sided_di
   #could also write health = rollDice(6) * rollDice(8) to simplifiy but it might be easier for them to break it into two parts!
   return health
+
+print(roll_6_and_8()) 
+#be sure to show the student wow we output it. Returning 'health' won't  actually show it. It just stores it for later. We need to print it to show it to the user!
