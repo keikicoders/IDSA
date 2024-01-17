698e6bb93a006f41d6de3c66dae0b3c82ad05412
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-17 13:25:09 -1000

    Update DiceRoll.py

--- IOLANI_S24/LessonSeven/DiceRoll.py
+++ IOLANI_S24/LessonSeven/DiceRoll.py
@@ -6,4 +6,5 @@ def roll_6_and_8():
   roll_6_sided_di = rollDice(6)
   roll_8_sided_di = rollDice(8)
   health = roll_6_sided_di * roll_8_sided_di
+  #could also write health = rollDice(6) * rollDice(8) to simplifiy but it might be easier for them to break it into two parts!
   return health
