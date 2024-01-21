f8dd46050e2be88b323189127427e4acfed72745
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-20 14:33:00 -1000

    Update PasswordLoginV2.py

--- IOLANI_S24/LessonEight/PasswordLoginV2.py
+++ IOLANI_S24/LessonEight/PasswordLoginV2.py
@@ -6,11 +6,9 @@ user_input_password = ''
 print("Welcome to the best app in the world. In order to enter you must enter your username and passoword")
 
 
-while True:
+while user_input_username != username and user_input_password != password:
   user_input_username = input("Username: ")
   user_input_password = input("Password: ")
   if user_input_username != username and user_input_password != password:
     print("Access denied! Try again")
-  else:
-    print("Access granted!")
-    break
+print("Access granted!")
