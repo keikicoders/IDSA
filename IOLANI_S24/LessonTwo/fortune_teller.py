4a18a77e58bc7596be1a3890252269772e355bd7
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-01-26 14:13:25 -1000

    Update and rename Blank.py to fortune_teller.py

---
+++ IOLANI_S24/LessonTwo/fortune_teller.py
@@ -0,0 +1,21 @@
+print("Hello I am Melvin, the greatest fortune teller of all time!")
+
+# -------- QUESTION 1--------------
+print("Ask me any yes or no question.")
+input(">")
+#We are hard coding in the response
+print("I can say with certainty, yes!")
+
+# -------- QUESTION 2--------------
+print("Ask me another question.")
+input(">")
+#We are hard coding in the response
+print("Hmmm...I am not sure. Better luck next time.")
+
+# -------- QUESTION 3--------------
+print("Ask me something else!")
+print("You can bet on it!")
+#We are hard coding in the response
+print("I am getting tired, so I will be leaving now. Goodbye!")
+      
+
