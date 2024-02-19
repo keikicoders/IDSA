78a48f1468243a1e8b733cebc322112ea5ef1734
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-02-18 15:15:47 -1000

    Update it_is_raining.py

--- IOLANI_S24/Conditionals/NestedIfs/it_is_raining.py
+++ IOLANI_S24/Conditionals/NestedIfs/it_is_raining.py
@@ -1,9 +1,9 @@
 is_raining = True
 have_umbrella = False
 
-if is_raining:
+if is_raining == True:
     print("It's raining outside!")
-    if have_umbrella:
+    if have_umbrella == True:
         print("You have an umbrella. Stay dry!")
     else:
         print("You don't have an umbrella. Better find some cover!")
