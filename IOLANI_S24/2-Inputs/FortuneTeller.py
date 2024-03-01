41c7cd19d7645f1a3e79302b384d71cb55dbc1de
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-03-01 16:00:15 -0500

    Update FortuneTeller.py

--- IOLANI_S24/2-Inputs/FortuneTeller.py
+++ IOLANI_S24/2-Inputs/FortuneTeller.py
@@ -1,1 +1,18 @@
+print("I am the greatest fortune teller of all time! Ask me a yes or no question!")
+
+#QUESTION 1
+input(">")
+print("You can bet on it!")
+
+#QUESTION 2
+print("Go ahead and ask me another yes or no question.")
+input(">")
+print("Hmm. I don't think so.")
+
+#QUESTION 3
+print("Ok one more question.")
+input(">")
+print("Absolutely!")
+
+print("It was nice meeting you. I must take my break now. Goodbye!")
 
