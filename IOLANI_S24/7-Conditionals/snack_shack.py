096b7463bcb6e429a9c3b00897381207b677ba56
Keiki Coders <150866570+keikicoders@users.noreply.github.com>
2024-03-01 16:28:11 -0500

    Update snack_shack.py

--- IOLANI_S24/7-Conditionals/snack_shack.py
+++ IOLANI_S24/7-Conditionals/snack_shack.py
@@ -45,16 +45,16 @@ print ( '''Welcome to Donuts 'R' Us! Here's our menu:
  - chocolate donuts
  - plain donuts
  ''' )
-donut_order = input ( "What kind of donuts do you want? " )
-number_ordered = input ( "How many donuts do you want to order? " )
-number_ordered = int ( number_ordered )
+donut_order = input("What kind of donuts do you want? ")
+number_ordered = input("How many donuts do you want to order? ")
+number_ordered = int(number_ordered)
 print()
 
 # Add bonus item for large order
 if number_ordered >= 12 :
   number_ordered += 1
-print ( "Customers who order 12 or more get an extra donut for free!" )
-print ( )
+  print("Customers who order 12 or more get an extra donut for free!")
+print()
 
 # Print order
 input ( "Press enter to receive your fresh donuts!" )
